b884cea00d3b436a1433f912451999769768d257
Bjørn Uhre Arnholtz <rugkiks@gmail.com>
2010-10-12 00:27:44 +0200

    Added support for editing amount

--- beercalc/tabs/expenses.py
+++ beercalc/tabs/expenses.py
@@ -28,6 +28,91 @@ class OptionsContainer(gtk.VBox):
         self.child_set_property(self.button_remove, "expand", False)
         self.child_set_property(self.button_edit, "expand", False)
 
+
+class ExpenseTable(gtk.ScrolledWindow):
+    def __init__(self):
+        super(type(self), self).__init__()
+        self.table = ExpenseTreeView()
+        self.add(self.table)
+
+class ExpenseTreeView(gtk.TreeView):
+    def __init__(self):
+        self.store = gtk.ListStore(gobject.TYPE_PYOBJECT, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_PYOBJECT)
+
+        super(type(self), self).__init__(model=self.store)
+        
+        self.set_property("rules-hint"   , True)
+        
+        self.set_property("enable-search", True)
+        self.set_property("search-column", 1   )
+        
+        self.col1 = gtk.TreeViewColumn()
+        self.col2 = gtk.TreeViewColumn()
+
+        self.cell1 = gtk.CellRendererText()
+        self.cell2 = AmountCell(self)
+
+        self.col1.pack_start(self.cell1, expand=True)
+        self.col2.pack_start(self.cell2, expand=False)
+        
+        self.cell1.connect("edited", self.OnEditDoneDesc)
+        
+        self.col1.set_property("title", u"Beskrivelse")
+        self.col1.set_property("expand", True)
+        self.col1.set_property("sizing", gtk.TREE_VIEW_COLUMN_AUTOSIZE)
+        self.cell1.set_property("xalign", 0)
+
+        self.col2.set_property("title", u"Pris")
+        self.col2.set_property("sizing", gtk.TREE_VIEW_COLUMN_AUTOSIZE)
+
+        self.cell1.set_property("editable", True)
+        
+        self.col1.add_attribute(self.cell1, 'text', 1)
+        self.col2.add_attribute(self.cell2, 'text', 2)
+        
+        self.append_column(self.col1)
+        self.append_column(self.col2)
+
+    def OnEditDoneDesc(self, cell, path, new_text):
+        iter = self.store.get_iter(path)
+        self.store.set(iter, 1, new_text)
+
+
+class AmountCell(gtk.CellRendererText):
+    def __init__(self, treeview):
+        super(type(self), self).__init__()
+        self.treeview = treeview
+        self.set_property("xalign", 1)
+        self.connect("edited", self.OnEditEnd)
+        self.connect("editing-started", self.OnEditStart)
+        self.set_property("editable", True)
+    
+    def OnEditStart(self, cell, entry, path):
+        text = entry.get_text()
+        text = text[:-4]
+        entry.set_text(text)
+
+    def OnEditEnd(self, cell, path, new_text):
+        new_text = new_text + "00"
+        if not all(x in "0123456789," for x in new_text):
+            return
+        
+        new_text = new_text.split(",")
+        if len(new_text) > 2:
+            return
+        amount = int("".join(new_text[0:1]) + "".join(new_text[1:2])[0:2])
+        
+        integer  = str(amount / 100)
+        fraction = str(amount % 100)
+        fraction = (fraction + "0")[:2]
+        
+        new_text = integer + "," + fraction + " kr."
+        
+        store = self.treeview.store
+        iter = store.get_iter(path)
+        store.set(iter, 2, new_text, 3, amount)
+
+
 class AddButton(gtk.Button):
     def __init__(self):
         super(type(self), self).__init__(u"_Tilføj")
@@ -35,7 +120,7 @@ class AddButton(gtk.Button):
 
     def OnClick(self, button):
         table = self.get_parent().get_parent().expenseTable.table
-        iter = table.store.append((uuid.uuid4().get_hex(), "", "0 kr.", 0))
+        iter = table.store.append((uuid.uuid4().get_hex(), "", "0,00 kr.", 0))
         table.set_cursor(table.store.get_path(iter), table.col1, True)
 
 class RemoveButton(gtk.Button):
@@ -54,60 +139,4 @@ class RemoveButton(gtk.Button):
 class EditButton(gtk.Button):
     def __init__(self):
         super(type(self), self).__init__(u"_Redigér")
-
-class ExpenseTable(gtk.ScrolledWindow):
-    def __init__(self):
-        super(type(self), self).__init__()
-        self.table = ExpenseTreeView()
-        self.add(self.table)
-
-class ExpenseTreeView(gtk.TreeView):
-    def OnEditDoneDesc(self, cell, path, new_text):
-        iter = self.store.get_iter(path)
-        self.store.set(iter, 1, new_text)
-
-    def OnEditDoneAmount(self, cell, path, new_text):
-        iter = self.store.get_iter(path)
-        self.store.set(iter, 2, new_text)
-
-    def __init__(self):
-        self.store = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_INT)
-        self.store.append((uuid.uuid4().get_hex(), u"Tuborg"     ,  "423,23 kr.", 42323))
-        self.store.append((uuid.uuid4().get_hex(), u"Guld tuborg", "2342,23 kr.", 242423))
-        self.store.append((uuid.uuid4().get_hex(), u"Sodavand",       "3,23 kr.", 323))
-
-        super(type(self), self).__init__(model=self.store)
-        
-        self.set_property("rules-hint"   , True)
-        
-        self.set_property("enable-search", True)
-        self.set_property("search-column", 1   )
-        
-        self.col1 = gtk.TreeViewColumn()
-        self.col2 = gtk.TreeViewColumn()
-
-        self.cell1 = gtk.CellRendererText()
-        self.cell2 = gtk.CellRendererText()
-
-        self.cell1.connect("edited", self.OnEditDoneDesc)
-        self.cell2.connect("edited", self.OnEditDoneAmount)
-        
-        self.col1.pack_start(self.cell1, expand=True)
-        self.col2.pack_start(self.cell2, expand=False)
-        
-        self.col1.set_property("title", u"Beskrivelse")
-        self.col1.set_property("expand", True)
-        self.cell1.set_property("xalign", 0)
-
-        self.col2.set_property("title", u"Pris")
-        self.cell2.set_property("xalign", 1)
-
-        self.cell1.set_property("editable", True)
-        self.cell2.set_property("editable", True)
-        
-        self.col1.add_attribute(self.cell1, 'text', 1)
-        self.col2.add_attribute(self.cell2, 'text', 2)
-        
-        self.append_column(self.col1)
-        self.append_column(self.col2)
 
