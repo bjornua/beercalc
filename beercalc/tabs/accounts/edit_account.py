9fcdfeca7fb94e57c944020f4d130187c0af7f49
Bjørn Uhre Arnholtz <rugkiks@gmail.com>
2010-11-01 12:34:35 +0100

    Made buttons prettier

--- beercalc/tabs/accounts/edit_account.py
+++ beercalc/tabs/accounts/edit_account.py
@@ -20,8 +20,17 @@ class AccountForm(gtk.VBox):
         gtk.VBox.__init__(self)
         cancel_button = CancelButton()
         save_button = SaveButton()
-        self.add(save_button)
-        self.add(cancel_button)
+        
+        buttons = gtk.HBox()
+        alignment_cancel = gtk.Alignment(xalign=1.0)
+        alignment_save = gtk.Alignment()
+        alignment_cancel.add(cancel_button)
+        alignment_save.add(save_button)
+        buttons.add(alignment_cancel)
+        buttons.add(alignment_save)
+        self.add(buttons)
+        buttons.child_set_property(alignment_save, "expand", False)
+
 
 class SaveButton(StockButton):
     def __init__(self):
@@ -33,7 +42,7 @@ class SaveButton(StockButton):
 
 class CancelButton(StockButton):
     def __init__(self):
-        StockButton.__init__(self, u"_Tilføj", gtk.STOCK_ADD)
+        StockButton.__init__(self, u"_Annullér", gtk.STOCK_CANCEL)
 
     def OnClick(self, button):
         AddDialog(self.get_toplevel())
